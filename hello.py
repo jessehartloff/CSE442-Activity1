dd020f3a8f9761173db5b1637f48975719744d0c
Jacob Ekstrum <jacobeks@buffalo.edu>
2017-09-08 14:41:17 -0400

    resolve things

--- hello.py
+++ hello.py
@@ -48,10 +48,10 @@ def goodbye():
 
 def why():
     print("Shouldn't a guy know how to use git by now come on fam")
-       print("Shouldn't a guy know how to use git by now come on fam")
-          print("Shouldn't a guy know how to use git by now come on fam")
-             print("Shouldn't a guy know how to use git by now come on fam")
-                print("Shouldn't a guy know how to use git by now come on fam")
+    print("Shouldn't a guy know how to use git by now come on fam")
+    print("Shouldn't a guy know how to use git by now come on fam")
+    print("Shouldn't a guy know how to use git by now come on fam")
+    print("Shouldn't a guy know how to use git by now come on fam")
                 
 
 def clown():
@@ -71,6 +71,7 @@ def fib(n):
 def helloMemeBoi():
     x = ""
     while True:
+        pass
 
 def goodbye():
     print("goodbye")
@@ -79,8 +80,8 @@ def helloMemeBoi():
     while True:
         print(x + "hello")
         print(x + "meme")
-         print(x + "boi")
-         x = " " + x 
+        print(x + "boi")
+        x = " " + x 
         print(x + "hello")
         print(x + "meme")
         print(x + "boi")
