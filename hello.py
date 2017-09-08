6496736e5b0b934523cb4defa20f9a0e0244972d
Alex <aca3936@gmail.com>
2017-09-08 14:11:46 -0400

    acarcher's commit

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 
 def hello():
     print("hello world!")
+    simplefunction()
+
+
+def simplefunction():
+    print("This is a simple assignment")
+    print("I hope everyone knows how to use python")
+    print("Good luck with your projects")
