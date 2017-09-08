979e3066be952b302afa441206ff591b37e89e61
Nick Martin <nmartin5@buffalo.edu>
2017-09-08 14:15:23 -0400

    First change, fixing conflicts

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 print("hello world!")
 print("hey world!")
 =======
 
 def hello():
-    print("hello world!")
->>>>>>> b09ebe095c3c366282d486316586bcded07ef484
+    print("I said hello world!")
