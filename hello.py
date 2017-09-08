a41a0b171070e79cff2bbc4bd65b3329e2810f0c
Brian Francis De Guzman <deguzmanbrianfrancis@gmail.com>
2017-09-08 14:29:26 -0400

    HelloHellov2

--- hello.py
+++ hello.py
@@ -34,6 +34,9 @@ def helloo():
 def hellohello():
 	print("Hello! Hello!")
 	print("I don't know why you say goodbye, I say hello")
+	print("I say high, you say low, You say why and I say I don't know!")
+
+
 
 def goodbye():
     print("goodbye")
