1c6c3da498b716bc9831aaf91ab4f246013079b2
vcng <vng251@yahoo.com>
2017-09-13 15:35:27 -0400

    jerry

---
+++ TomandJerry.py
@@ -0,0 +1,91 @@
+print("hello world!!")
+print("hey world!")
+print("i hate git")
+print("omg")
+print("i hate this")
+print("why you do ths")
+print("am i done?")
+print("almost there")
+print("need to change this file again because adding one in class didnt add me as a contriubuter")
+print("exluding merges?")
+
+def hello():
+    print("hello world!")
+    simplefunction()
+
+def waddupp():
+    print("---------------------------------")
+    print("this is for credit")
+    print("idk what else to put")
+    print("only a couple more lines")
+    print("this will be a mess to read")
+    print("how about a line break")
+    print("---------------------------------")
+
+def simplefunction():
+    print("This is a simple assignment")
+    print("I hope everyone knows how to use python")
+    print("Good luck with your projects")
+
+def helpme():
+    print("---------------------------------")
+    print("I pushed 5 times and I am not in contributors")
+    print("What is going on?")
+    print("This is my 6th time pushing")
+    print("Please be successful")
+    simplefunction()
+    wadddup()
+    hello()
+
+def hellohello():
+    print("Hello! Hello!")
+    print("I don't know why you say goodbye, I say hello")
+    print("I say high, you say low, You say why and I say I don't know!")
+
+
+
+def goodbye():
+    print("goodbye")
+
+def why():
+    print("Shouldn't a guy know how to use git by now come on fam")
+    print("Shouldn't a guy know how to use git by now come on fam")
+    print("Shouldn't a guy know how to use git by now come on fam")
+    print("Shouldn't a guy know how to use git by now come on fam")
+    print("Shouldn't a guy know how to use git by now come on fam")
+                
+
+def clown():
+    i = 0
+    while i < 999999999:
+        print("dank memes")
+        print("donkey kong")
+        print("skdjfnkjsldfhnjklsadfhsjldf")
+        i=+1
+
+#not efficient but works
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-2) + fib(n-1)
+
+def helloMemeBoi():
+    x = ""
+    while True:
+        print(x + "hello")
+        print(x + "meme")
+        print(x + "boi")
+        x = " " + x 
+
+def goodbye():
+    print("goodbye world!")
+
+
+def benchmark():
+    while(True):
+        print("PUSHING THE LIMIT")
+        print("NOT YOUR GRANDMA'S BENCHMARK!!!")
+        print("THROW SOME ICE ON THAT CPU CUZ THIS IS FIRE")
+        if 2 + 2 == 5:
+            break # this should never happen
+
